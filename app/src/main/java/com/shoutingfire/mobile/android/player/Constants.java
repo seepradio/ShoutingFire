349f2ba05caffc0363f307960a0deb78c26a62ce
seepradio <seepradio@users.noreply.github.com>
2019-03-18 23:39:41 -0400

    Update Constants.java

--- app/src/main/java/com/shoutingfire/mobile/android/player/Constants.java
+++ app/src/main/java/com/shoutingfire/mobile/android/player/Constants.java
@@ -1,13 +1,19 @@
 /**
  * Defines all app and music stream specific values.
  *
- * Hopefully simplifies building multiple player apps
- * from the same, diffable source code trees.
- * Addiions:
- * - Edit each source file and update the package statement.
- * - Edit AndroidManifest.xml and update package name.
+ * Copyright shoutingfire.com 2018,2019
  *
- * Copyright (c) 2018 bmir.org and shoutingfire.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.shoutingfire.mobile.android.player;
 
@@ -17,11 +23,14 @@ public class Constants {
     public static final String PACKAGE_NAME = "com.shoutingfire.mobile.android.player";
     public static final String APP_NAME_LOWER = "shoutingfire";
     public static final String APP_NAME_MIXED = "ShoutingFire";
+    public static final String APP_NAME_VERBOSE = "ShoutingFire";
     public static final String APP_NAME_UPPER = "SHOUTINGFIRE";
+    public static final int NOTIFICATION_ID = 136342145;
+    public static final String NOTIFICATION_CHANNEL_ID = "shoutingfirenotification";
 
     // Target stream.
     public static final String MEDIA_HOSTNAME = "shoutingfire-ice.streamguys1.com";
-    public static final String MEDIA_URL_STRING = "http://" + MEDIA_HOSTNAME + ":80/live";
+    public static final String MEDIA_URL_STRING = "https://" + MEDIA_HOSTNAME + ":80/live";
     public static final String STATUS_URL_STRING = "http://" + MEDIA_HOSTNAME + "/";
 
     // Image references.
@@ -29,8 +38,4 @@ public class Constants {
     public static final int IMG_DOTS = R.drawable.shoutingfiredots;
     public static final int IMG_PLAY = R.drawable.shoutingfireplay;
     public static final int IMG_STOP = R.drawable.shoutingfirestop;
-
-    // Notifications.
-    ////public static final int NOTIFICATION_ID = 947392519; // BMIR
-    public static final int NOTIFICATION_ID = 136342145; // ShoutingFire
 }
